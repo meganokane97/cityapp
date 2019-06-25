0b022919ae757ff7bed7e21967fc507624f68afc
Megan OKane <megano@kainos.com>
2019-06-25 15:27:07 +0100

    changes to method

--- src/app/add-city/add-city.component.ts
+++ src/app/add-city/add-city.component.ts
@@ -11,21 +11,20 @@ export class AddCityComponent implements OnInit {
   public newCity:City;
   data:DataService;
   constructor(dataService:DataService) {
-    this.data = dataService;
+    this.data = dataService
    }
 
   ngOnInit() {
-    this.newCity = new City();
+    this.newCity = new City()
   }
 
   addCity(addForm): void{
     if(addForm.valid){
-      var cityToAdd = this.newCity;
-      this.newCity = new City();
-      this.data.cities.push(cityToAdd);
+      var cityToAdd = this.newCity
+      this.newCity = new City()
+      this.data.cities.push(cityToAdd)
     }else{
-      console.error("Add City form is in an invalid state");
+      console.error("Add City form is in an invalid state")
     }
   }
-
 }
